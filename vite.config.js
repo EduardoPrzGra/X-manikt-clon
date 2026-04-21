e2a45e92ef3ac846de6c9536ec4d44b8fb7490f7
Eduardo Perez Garcia <107895480+EduardoPrzGra@users.noreply.github.com>
2026-04-20 19:44:50 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/X-manikt-/',
+  // Asegúrate de que el nombre sea el mismo que el del repo en tu cuenta
+  base: '/X-manikt-clon/', 
 })
